c418b53d1a367b922b173c8d07fd92e16d39fc40
hoanhduc.nguyen <hoanh-duc-trung.nguyen@etu.hesge.ch>
2021-12-16 15:46:16 +0100

    feafaw

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,10 +22,11 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: purple;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  background-color: purple;
 }
 </style>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="top_navbar">
-        <div class="Title">Introduction à l'électrodynamique</div>
-        <div class="Author">par Mr.Pump</div>
-        <div class="Timer"> 4:59</div>
-        <div class="credit"> 0,00</div>
+        <div class="class_information">
+            <p class="Title">Introduction à l'électrodynamique</p>
+            <p> par </p>
+            <p class="Author">Mr.Pump</p>
+        </div>
+        <div class="user_information">
+            <p class="Timer"> 4:59 </p>
+            <p class="credit"> 0,00</p>
+        </div>
     </div> 
 </template>
 
@@ -13,13 +18,35 @@
     overflow: hidden;
     background-color: purple;
     display: flex;
+    justify-content: space-between;
+}
+
+.class_information{
+    display: flex;
+    color: white;
+}
+.class_information p {
+    margin: 0px 0px 0px 0px;
+    padding: 1vh 1vh 1vh 1vh;
+}
+
+.user_information {
+    display: flex;
+    color: white;
 }
 
 .Title {
-  padding: 3% 3% 3% 3%;
   font-weight: bold;
-  color: white;
+  text-decoration: underline;
+  font-size: 4vh;
 }
+
+.Author {
+    bottom: 0;
+    text-decoration: underline;
+}
+
+
 
 
 
