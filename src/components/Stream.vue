6b93e871996cf074358ae130ab459f79cbbcb2fe
antwan <antoine.blancy@etu.hesge.ch>
2022-04-21 18:36:12 +0200

    Dynamic stream getting Done

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,137 +1,125 @@
 <template>
   <div>
     <div :style="CollapseStream" class="streaming">
-
-      <video-player class="vjs-custom-skin" ref="videoPlayer" :options="playerOptions" @ready="onPlayerReadied" @timeupdate="onTimeupdate">
+      <video-player
+        class="vjs-custom-skin"
+        ref="videoPlayer"
+        :options="playerOptions"
+        @ready="onPlayerReadied"
+        @timeupdate="onTimeupdate"
+      >
       </video-player>
-
-      
     </div>
     <Chat @collapse="collapse"></Chat>
   </div>
 </template>
 
 <script>
-  import videojs from 'video.js'
-  import Chat from './Chat.vue'
+import videojs from "video.js";
+import Chat from "./Chat.vue";
+import Vue from "vue";
 
-  const BACKEND_URL ="http://127.0.0.1:8080";
-  const myHeader = new Headers({
-    'Access-Control-Allow-Origin': 'http://localhost:8080',
-    //'Access-Control-Allow-Origin': 'http://localhost:8081',
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  });
+const myHeader = new Headers({
+  "Access-Control-Allow-Origin":
+    "http://" + Vue.prototype.$BACKENDURL + ":8080",
+  Accept: "application/json",
+  "Content-Type": "application/json",
+});
 
-async function getStreamUrl(){
-  let url = window.location.href;
-  let arr = url.split("/");
-  let profID = arr[arr.length - 1];
-  
-   return fetch(BACKEND_URL+ "/stream/get/"+ profID + "/userName",{ method: 'get', headers: myHeader})
-      .then(res=>{
-          return res.json().then(o=>{
-            return o["url"];
-          })
-      });
-}
+// hls plugin for videojs6
+/*require('videojs-contrib-hls/dist/videojs-contrib-hls.js') cette ligne casse le programme*/
+    window.videojs = videojs;
 
-
-  getStreamUrl().then( a=>{
-    console.log("OOOOOOOOOOOOOH")
-    var streamSource = a;                                                                                                                                                                                                                                                               
-    console.log(streamSource)
-    this.sources.src = a;
-  })
-
-
-  window.videojs = videojs
-  // hls plugin for videojs6
-  /*require('videojs-contrib-hls/dist/videojs-contrib-hls.js') cette ligne casse le programme*/
-
-
-
-
-
-
-
-
-
-  export default {
+export default {
   components: { Chat },
-    data() {
-      return {
+  data() {
+    return {
       isOpen: true,
       initialized: false,
-      currentTech: '',
+      currentTech: "",
       streams: {
-        rtmp: '',
+        rtmp: "",
       },
       playerOptions: {
-        // videojs and plugin options 
+        // videojs and plugin options
         preferFullWindow: true,
         responsive: true,
         autoSetup: true,
-        aspectRatio : "16:9",
+        aspectRatio: "16:9",
 
         autoplay: true,
         controls: true,
-        techOrder: ['flash', 'html5'],
-        sourceOrder: true,             
+        techOrder: ["flash", "html5"],
+        sourceOrder: true,
         muted: true,
-        language: 'en',
-        flash: { hls: { withCredentials: false }},
-        html5: { hls: { withCredentials: false }}, 
+        language: "en",
+        flash: { hls: { withCredentials: false } },
+        html5: { hls: { withCredentials: false } },
         sources: [
           {
             withCredentials: false,
             type: "application/x-mpegURL",
             src: "",
-          }
+          },
         ],
         poster: "https://i.ibb.co/4R5J8G6/Dessin-sans-titre-3-1.png",
-        notSupportedMessage: " This video is temporarily unplayable. Please try again later ",
-
-      }
-    }
+        notSupportedMessage:
+          " Le stream n'est pas disponnible. Veuillez réessayer plus tard.",
+      },
+    };
+  },
+  methods: {
+    getStreamUrl() {
+      return fetch(
+        "http://" +
+          Vue.prototype.$BACKENDURL +
+          ":8080/stream/get/" +
+          Vue.prototype.$roomID +"/"+
+          Vue.prototype.$userID,
+        { method: "get", headers: myHeader }
+      ).then((res) => {
+        return res.json().then((o) => {
+          return o["url"];
+        });
+      });
     },
-    methods: {
-      clearSource(){
-        this.sources.src = "";
-      },
-
-      onPlayerReadied() {
-        if (!this.initialized) {
-          this.initialized = true
-          this.currentTech = this.player.techName_
-        }
-      },
-      // record current time
-      onTimeupdate() {
-      },
-      collapse(e) {
-        this.isOpen = e
-      }
-    },  mounted(){
-      const thisInstance = this
-      this.$root.$on('clearSource', function(){
-      thisInstance.clearSource()
-    })},
-    computed :{
-      CollapseStream() {
-        if(this.isOpen) {
-          return {
-            width: '80%'
-          }
-        }else{
-          return {
-            width: '100%'
-          }
-        }
+    onPlayerReadied() {
+      if (!this.initialized) {
+        this.initialized = true;
+        this.currentTech = this.player.techName_;
       }
     },
-  }
-  
+    // record current time
+    onTimeupdate() {},
+    collapse(e) {
+      this.isOpen = e;
+    },
+  },
+  beforeMount() {
+    this.getStreamUrl().then((a) => {
+      console.log("OOOOOOOOOOOOOH");
+      var streamSource = a;
+      console.log(streamSource);
+      this.playerOptions.sources[0].src = a;
+    });
+    //this.forceRefresh();
+  },
+  mounted() {
+  },
+  computed: {
+    CollapseStream() {
+      if (this.isOpen) {
+        return {
+          width: "80%",
+        };
+      } else {
+        return {
+          width: "100%",
+        };
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -145,7 +133,7 @@ h2 {
   padding-top: 3%;
   color: white;
 }
-@media only screen and (max-width: 800px){
+@media only screen and (max-width: 800px) {
   .streaming {
     width: 100% !important;
     height: 100% !important;
