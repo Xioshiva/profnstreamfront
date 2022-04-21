f8934180cb322279327a5bc1c82633475a4a3762
antwan <antoine.blancy@etu.hesge.ch>
2022-04-21 15:58:44 +0200

    Refactoring

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueVideoPlayer from 'vue-video-player'
+import io from "socket.io-client";
 
 Vue.config.productionTip = false
 Vue.use(VueVideoPlayer)
 
+Vue.prototype.$userID = prompt("prompt", "textbox's intial text");
+Vue.prototype.$io = io;
+Vue.prototype.$roomID = location.href.substring(location.href.lastIndexOf('/')+1);
 
 new Vue({
   render: h => h(App),
